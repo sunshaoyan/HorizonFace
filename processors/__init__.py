c27263e37df4187795b3f231e4336634b0ea9164
11648169992 <1648169992@qq.com>
2017-09-19 13:22:12 +0800

    add glasses

--- processors/__init__.py
+++ processors/__init__.py
@@ -1,7 +1,9 @@
 from .LikeTextProcessor import LikeTextProcessor
 from .BeautifulModelProcessor import BeautifulModelProcessor
+from .GlassesNumberProcessor import GlassesNumberProcessor
 
 __all__= [
     'LikeTextProcessor',
     'BeautifulModelProcessor',
+    'GlassesNumberProcessor',
 ]
